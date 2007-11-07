14ae2ee821c494459f118d9eddf510959331ff20
ChenZaichun <ChenZaichun@73f62926-e13a-0410-b1cd-f1dd3608a682>
2007-11-07 00:04:27 +0000

    add gxfont to rep

---
+++ trunk/src/GXFont.h
@@ -0,0 +1,22 @@
+
+/*!
+ * \file	GXFont.h
+ * \brief	WFont lua bind
+ *
+ * $Id: $
+ *
+ * CopyRight (c) GearX Team, 2007
+ * See LISENCE for more information
+ */
+
+#ifndef _GX_FONT_H
+#define _GX_FONT_H
+
+#include "GXLua.hpp"
+#include "GXInc.h"
+
+
+int luaopen_FontLib(lua_State* L);
+
+
+#endif
